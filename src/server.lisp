23bff2ae7b12d82e69d3ed42c44ce7dda313cbc9
goosecoid <adblavier@gmail.com>
2024-08-28 21:22:14 +0200

    fix: single main entrypoint

--- src/server.lisp
+++ src/server.lisp
@@ -1,17 +1,13 @@
 (in-package :ptp-bookmarks-ui)
 
-;; bootsrap db
-(bootstrap-db)
-
 (defparameter *svg-url*
   "https://raw.githubusercontent.com/n3r4zzurr0/svg-spinners/abfa05c49acf005b8b1e0ef8eb25a67a7057eb20/svg-css/6-dots-rotate.svg")
-(defparameter *movies-plist* (list-all-movies-as-plist))
 (defparameter *app* (make-instance 'ningle:app))
 
 (defun bootstrap-routes (app-instance)
 
   (setf (ningle:route app-instance "/")
-        (movie-list *movies-plist*))
+        (movie-list (list-all-movies-as-plist)))
 
   (setf (ningle:route app-instance "/filter-genre")
         #'(lambda (params)
@@ -26,11 +22,9 @@
         #'(lambda (params)
             (spinneret:with-html-string ()
               (:div :id "trailer-urls"
-                    (:raw (get-trailer-links-el (cdar params)))))))
+                    (:raw (get-trailer-links-el (cdar params))))))))
 
-  )
-
-(bootstrap-routes *app*)
-
-(defparameter *server* (clack:clackup *app* :port 8080))
-;; (clack:stop *server*)
+(defun main ()
+  (bootstrap-db)
+  (bootstrap-routes *app*)
+  (clack:clackup *app* :port 8080))
